80a8adbb660623f8c182f4f82699f74c360d00e9
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-06-22 14:09:23 +0100

    Portfolio update with southside project

--- src/Components/Portfolio/Portfolio_data.js
+++ src/Components/Portfolio/Portfolio_data.js
@@ -1,58 +1,72 @@
 const Portfolio_data = [
-    {
-        id: 1,
-        category: 'WEB DESIGN',
-        totalLikes: "280",
-        title: "A static Clone of my Visual Studio Code Window",
-        image: "/images/vs-clone.png",
-        about: "Some crazy idea I had while developing another project to try rendering my workspace window in HTML ans CSS only, pretty much like a screenshot of my window in my browser. This was developed for as desktop view so don't expect too much if you're using a mobile device.",
-        link: 'https://tohirbabs.github.io/visual-studio-screenshot/',
-    },
-    {
-        id: 2,
-        category: 'WEB DESIGN',
-        totalLikes: "143",
-        title: "A Nintendo like UI of the wordle game",
-        image: "/images/gameboy-wordle.png",
-        about: 'A fresh take on the UI of the very popular wordle game, which is just my way of making it a little bit more interesting with the classic nintendo nostalgic feel. the design is mobile view biased but im working on its responsiveness',
-        link:'https://tohirbabs.github.io/gameboy_wordle/',
-    },
-    {
-        id: 3,
-        category: 'WEB DESIGN',
-        totalLikes: "160",
-        title: "A music player site with minimalistic design",
-        image: "/images/music-player.png",
-        about: 'A minimalistic and light weight music playing site that plays songs from a local storage and has cool effects.',
-        link:'https://tohirbabs.github.io/MUSIC-PLAYER/',
-    },
-    {
-        id: 4,
-        category: 'WEB DESIGN',
-        totalLikes: "200",
-        title: "Conway's game of life in action with classic nintendo UI",
-        image: "/images/conway.png",
-        about: "A nice implementation of the 0 player game that demonstrates how simple rules can evolve into complex patterns, some of them even looking like life. In the classic nintendo UI of course.",
-        link:'https://conwaygof.netlify.app/',
-    },
-    {
-        id: 5,
-        category: 'WEB DESIGN',
-        totalLikes: "277",
-        title: "An HTML & CSS only clone of my twitter homepage",
-        image: "/images/twitter-homepage.png",
-        about: 'A responsive rendering of a snapshot of my twitter homepage.',
-        link:'https://tohirbabs.github.io/twitter_homepage/',
-    },
-    {
-        id: 6,
-        category: 'WEB DESIGN',
-        totalLikes: "338",
-        title: "A landing page for mobile site",
-        image: "/images/easybank.png",
-        about: '',
-        link:'https://easybankhomepage.netlify.app/#',
-    },
-]
+  {
+    id: 1,
+    category: "FRONTEND DEVELOPMENT",
+    totalLikes: "338",
+    title: "An E-Commerce Site for a Restaurant",
+    image: "/images/southside.png",
+    about: "",
+    link: "https://southsidefood.vercel.app/",
+  },
 
-export default Portfolio_data
+  {
+    id: 2,
+    category: "WEB DESIGN",
+    totalLikes: "143",
+    title: "A Nintendo like UI of the wordle game",
+    image: "/images/gameboy-wordle.png",
+    about:
+      "A fresh take on the UI of the very popular wordle game, which is just my way of making it a little bit more interesting with the classic nintendo nostalgic feel. the design is mobile view biased but im working on its responsiveness",
+    link: "https://tohirbabs.github.io/gameboy_wordle/",
+  },
+  {
+    id: 3,
+    category: "WEB DESIGN",
+    totalLikes: "160",
+    title: "A music player site with minimalistic design",
+    image: "/images/music-player.png",
+    about:
+      "A minimalistic and light weight music playing site that plays songs from a local storage and has cool effects.",
+    link: "https://tohirbabs.github.io/MUSIC-PLAYER/",
+  },
+  {
+    id: 4,
+    category: "WEB DESIGN",
+    totalLikes: "200",
+    title: "Conway's game of life in action with classic nintendo UI",
+    image: "/images/conway.png",
+    about:
+      "A nice implementation of the 0 player game that demonstrates how simple rules can evolve into complex patterns, some of them even looking like life. In the classic nintendo UI of course.",
+    link: "https://conwaygof.netlify.app/",
+  },
+  {
+    id: 5,
+    category: "WEB DESIGN",
+    totalLikes: "277",
+    title: "An HTML & CSS only clone of my twitter homepage",
+    image: "/images/twitter-homepage.png",
+    about: "A responsive rendering of a snapshot of my twitter homepage.",
+    link: "https://tohirbabs.github.io/twitter_homepage/",
+  },
+  {
+    id: 6,
+    category: "WEB DESIGN",
+    totalLikes: "338",
+    title: "A landing page for mobile site",
+    image: "/images/easybank.png",
+    about: "",
+    link: "https://easybankhomepage.netlify.app/#",
+  },
+  {
+    id: 7,
+    category: "WEB DESIGN",
+    totalLikes: "280",
+    title: "A static Clone of my Visual Studio Code Window",
+    image: "/images/vs-clone.png",
+    about:
+      "Some crazy idea I had while developing another project to try rendering my workspace window in HTML ans CSS only, pretty much like a screenshot of my window in my browser. This was developed for as desktop view so don't expect too much if you're using a mobile device.",
+    link: "https://tohirbabs.github.io/visual-studio-screenshot/",
+  },
+];
+
+export default Portfolio_data;

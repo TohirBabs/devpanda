edc23607c607f24466b6f493bfb4873b1ceef1d9
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2022-06-29 20:48:33 +0100

    updates

--- src/Components/Hero/Home.js
+++ src/Components/Hero/Home.js
@@ -1,102 +1,114 @@
-import React from 'react'
-import "./Home.css"
+import React from "react";
+import "./Home.css";
 
-import hero from "./hero.jpg"
-import html from "./icons/html.png"
-import css from "./icons/css.png"
-import js from "./icons/js.png"
-import sass from "./icons/sass.png"
-import react from "./icons/react.png"
+import hero from "./hero.jpg";
+import html from "./icons/html.png";
+import css from "./icons/css.png";
+import js from "./icons/js.png";
+import sass from "./icons/sass.png";
+import next from "./icons/next.svg";
+import react from "./icons/react.png";
 
-
-import { Typewriter, useTypewriter, Cursor } from 'react-simple-typewriter'
+import { Typewriter, useTypewriter, Cursor } from "react-simple-typewriter";
 // import { LoremIpsum } from 'react-lorem-ipsum';
 
 const Home = () => {
   return (
     <>
-      <section className='hero' id='home'>
-        <div className='container f_flex top'>
-          <div className='left top'>
-            <h3 className='uppercase'>welcome to my world</h3>
-            <h1> 
+      <section className="hero" id="home">
+        <div className="container f_flex top">
+          <div className="left top">
+            <h3 className="uppercase">welcome to my world</h3>
+            <h1>
               Hi, I'm <span>Tohir Babs</span>
             </h1>
 
             <h2>
-              a(n) <span>
-                  <Typewriter
-                    words={['Web Designer.', 'Frontend Dev.', 'Blogger.', 'Engr Student.', 'CAD Designer.']}
-                    loop
-                    cursor
-                    cursorStyle='|'
-                    typeSpeed={70}
-                    deleteSpeed={50}
-                    delaySpeed={1000}
-                  />
+              a(n){" "}
+              <span>
+                <Typewriter
+                  words={[
+                    "Web Designer.",
+                    "Frontend Dev.",
+                    "Blogger.",
+                    "Engr Student.",
+                    "CAD Designer.",
+                  ]}
+                  loop
+                  cursor
+                  cursorStyle="|"
+                  typeSpeed={70}
+                  deleteSpeed={50}
+                  delaySpeed={1000}
+                />
               </span>
             </h2>
 
             <p>
-              I'm a 21 year old with ever burning curiosity and a deep fascinaton with web technologies which
-              motivates me to build and develop web applications that optimizes user experiences with minimalist principles 
+              I'm a 21 year old with ever burning curiosity and a deep
+              fascinaton with web technologies which motivates me to build and
+              develop web applications that optimizes user experiences with
+              minimalist principles
             </p>
 
-            <div className='hero_btn d_flex'>
-              <div className='col_1'>
-                <h4 className='uppercase'>find me with</h4>
-                <div className='button'>
-                <a href='https://twitter.com/BabsTohir'>
-                  <button className='btn_shadow'>
-                    <i className='fab fa-twitter'></i>
-                  </button>
+            <div className="hero_btn d_flex">
+              <div className="col_1">
+                <h4 className="uppercase">find me with</h4>
+                <div className="button">
+                  <a href="https://twitter.com/BabsTohir">
+                    <button className="btn_shadow">
+                      <i className="fab fa-twitter"></i>
+                    </button>
                   </a>
-                  <a href='https://github.com/TohirBabs'>
-                  <button className='btn_shadow'>
-                    <i className='fab fa-github'></i>
-                  </button>
+                  <a href="https://github.com/TohirBabs">
+                    <button className="btn_shadow">
+                      <i className="fab fa-github"></i>
+                    </button>
                   </a>
-                  <a href='https://www.linkedin.com/in/tohir-babs-6a0045167/'>
-                  <button className='btn_shadow'>
-                  <i className='fab fa-linkedin-in'></i>
-                  </button>
+                  <a href="https://www.linkedin.com/in/tohir-babs-6a0045167/">
+                    <button className="btn_shadow">
+                      <i className="fab fa-linkedin-in"></i>
+                    </button>
                   </a>
                 </div>
               </div>
 
-              <div className='col_2'>
-                <h4 className='uppercase'>best skill on</h4>
+              <div className="col_2">
+                <h4 className="uppercase">best skill on</h4>
 
-                <a href='https://tohirbabs.github.io/gameboy_wordle/'>
-                  <button className='btn_shadow'>
-                      <img src={js} alt='' />
+                <a href="https://tohirbabs.github.io/gameboy_wordle/">
+                  <button className="btn_shadow">
+                    <img src={js} alt="" />
                   </button>
                 </a>
-                <a href='https://easybankhomepage.netlify.app/'>
-                  <button className='btn_shadow'>
-                      <img src={sass} alt='' />
+                <a href="https://easybankhomepage.netlify.app/">
+                  <button className="btn_shadow">
+                    <img src={sass} alt="" />
                   </button>
                 </a>
-                <a href='https://conwaygof.netlify.app/'>
-                  <button className='btn_shadow'>
-                      <img src={react} alt='' />
+                <a href="https://conwaygof.netlify.app/">
+                  <button className="btn_shadow">
+                    <img src={react} alt="" />
                   </button>
                 </a>
-
+                <a href="https://southsidefood.vercel.app/">
+                  <button className="btn_shadow">
+                    <img src={next} alt="" />
+                  </button>
+                </a>
               </div>
             </div>
           </div>
 
-          <div className='right'>
-            <div className='right_img btn_shadow'>
-              <img src={hero} alt='' />
+          <div className="right">
+            <div className="right_img btn_shadow">
+              <img src={hero} alt="" />
             </div>
           </div>
-
         </div>
       </section>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
--- src/Components/Resume/ResumeApi.js
+++ src/Components/Resume/ResumeApi.js
@@ -1,36 +1,45 @@
 const ResumeApi = [
-    {
-        id: 1,
-        category: 'education',
-        year: 'University of lagos (2017-2023)',
-        title: 'Mechanical Engineering',
-        // desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Imperdiet erat nullam tortor quis elit lacus blandit vitae. Nostra dapibus bibendum; curae magnis commodo metus vestibulum tristique. Tristique",
-        rate: '4.01/5'
-    },
-    {
-        id: 2,
-        category: 'education',
-        year: 'MIT CourseWare (2022-Present)',
-        title: 'Computer Science',
-        // desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Imperdiet erat nullam tortor quis elit lacus blandit vitae. Nostra dapibus bibendum; curae magnis commodo metus vestibulum tristique. Tristique",
-        rate: '4.5/5'
-    },
-    {
-        id: 3,
-        category: 'experience',
-        year: 'EkoKonnect(2018-2019)',
-        title: 'Software Engineering',
-        // desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Imperdiet erat nullam tortor quis elit lacus blandit vitae. Nostra dapibus bibendum; curae magnis commodo metus vestibulum tristique. Tristique",
-        rate: '4.9/5'
-    },
-    {
-        id: 4,
-        category: 'experience',
-        year: 'PAADC (2020-Present)',
-        title: 'Frontend dev',
-        // desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Imperdiet erat nullam tortor quis elit lacus blandit vitae. Nostra dapibus bibendum; curae magnis commodo metus vestibulum tristique. Tristique",
-        rate: '4.8/5'
-    },
-]
+  {
+    id: 1,
+    category: "education",
+    year: "University of lagos (2017-2023)",
+    title: "Mechanical Engineering",
+    // desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Imperdiet erat nullam tortor quis elit lacus blandit vitae. Nostra dapibus bibendum; curae magnis commodo metus vestibulum tristique. Tristique",
+    rate: "4.01/5",
+  },
+  {
+    id: 2,
+    category: "education",
+    year: "MIT CourseWare (2022-Present)",
+    title: "Computer Science",
+    // desc: "Lorem ipsum odor amet, consectetuer adipiscing elit. Imperdiet erat nullam tortor quis elit lacus blandit vitae. Nostra dapibus bibendum; curae magnis commodo metus vestibulum tristique. Tristique",
+    rate: "4.5/5",
+  },
+  {
+    id: 3,
+    category: "experience",
+    year: "EkoKonnect(2018-2019)",
+    title: "Software Engineering",
+    desc: "Learning as an intern at the EkoKonnect workshops I learned the basics of procedural programming while experimenting with the finch robot and programming it to perform various complex tasks.",
+    rate: "4.9/5",
+  },
 
-export default ResumeApi
+  {
+    id: 5,
+    category: "experience",
+    year: "MOVEABLES (2021- 2022)",
+    title: "Web Design and development",
+    desc: "Worked in close contact with the startup founder of MOVEABLES on designing and developing the concepts and flow of the landing pages for the website. ",
+    rate: "4.8/5",
+  },
+  {
+    id: 6,
+    category: "experience",
+    year: "SOUTHSIDEFOODS (2022- )",
+    title: "Web Application development",
+    desc: "while working in a widespresd team, I was tasked with developing the web application for an e-commerce enterprise from figma designs while in constant communication with the backend team on easy integration of api functionalities, and great user experience.",
+    rate: "4.8/5",
+  },
+];
+
+export default ResumeApi;

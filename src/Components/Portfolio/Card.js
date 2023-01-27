e67fdbfa6748a0dc1598d7364221ef40ed3f5fba
dev_panda <75636672+TohirBabs@users.noreply.github.com>
2023-01-27 10:22:49 +0100

    card

--- src/Components/Portfolio/Card.js
+++ src/Components/Portfolio/Card.js
@@ -1,58 +1,61 @@
-import React, {useState} from 'react'
+import React, { useState } from "react";
 
 const Card = (props) => {
-  const [modal, setModal] = useState(false)
+  const [modal, setModal] = useState(false);
 
-  const toggleModal = () =>{
-      setModal(!modal)
-  }
+  const toggleModal = () => {
+    setModal(!modal);
+  };
   return (
     <>
-    <div className='box btn_shadow'>
-      <div className='img'>
-        <img src={props.image} alt='' onClick={toggleModal}/>
-      </div>
-      <div className='category uppercase d_flex'>
-        <span onClick={toggleModal}>{props.category}</span>
-      </div>
-      
-      <div className='title'>
-        <h2 onClick={toggleModal}>{props.title} </h2>
-      </div>
-         
-    </div>
+      <div className="box btn_shadow">
+        <div className="img">
+          <img src={props.image} onClick={toggleModal} />
+        </div>
+        <div className="category uppercase d_flex">
+          <span onClick={toggleModal}>{props.category}</span>
+        </div>
 
+        <div className="title">
+          <h2 onClick={toggleModal}>{props.title} </h2>
+        </div>
+      </div>
 
-
-    {modal &&(
-      <div className='modal'>
-        <div className='overlay'>
-          <div className='modal-content d_flex'>
-            <div className='modal-img left'>
-              <img src={props.image} alt="" />
-            </div>
-            <div className='modal-text right'>
-              <span>Featured - Design</span>
-              <h1>{props.title}</h1>
-              <p>{props.about}</p>
-            <div className='button f_flex mtop'>
-              <button className='btn btn_shadow uppercase'>
-                like <i className='far fa-thumbs-up'></i>
-              </button>
-              <button className='btn btn_shadow uppercase' onClick={()=> window.open(props.link, "_blank")}>
-                view project <i className='fas fa-chevron-right'></i>
-              </button>
-            </div>
-            <button className='close-modal btn_shadow' onClick={toggleModal}>
-              <i className='fas fa-times '></i>
-            </button>
+      {modal && (
+        <div className="modal">
+          <div className="overlay">
+            <div className="modal-content d_flex">
+              <div className="modal-img left">
+                <img src={props.image} alt="" />
+              </div>
+              <div className="modal-text right">
+                <span>Featured - Design</span>
+                <h1>{props.title}</h1>
+                <p>{props.about}</p>
+                <div className="button f_flex mtop">
+                  <button className="btn btn_shadow uppercase">
+                    like <i className="far fa-thumbs-up"></i>
+                  </button>
+                  <button
+                    className="btn btn_shadow uppercase"
+                    onClick={() => window.open(props.link, "_blank")}
+                  >
+                    view project <i className="fas fa-chevron-right"></i>
+                  </button>
+                </div>
+                <button
+                  className="close-modal btn_shadow"
+                  onClick={toggleModal}
+                >
+                  <i className="fas fa-times "></i>
+                </button>
+              </div>
             </div>
           </div>
         </div>
-      </div>
-    )}
+      )}
     </>
-  )
-}
+  );
+};
 
-export default Card
+export default Card;
